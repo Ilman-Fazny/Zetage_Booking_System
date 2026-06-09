679413de374a0fe5a76324cc2a0857e0acf60c38
Ilman Fazny <83239298+Ilman-Fazny@users.noreply.github.com>
2026-06-10 03:40:46 +0530

    fix: Error

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,17 +1,15 @@
+import bcrypt
 from datetime import datetime, timedelta
-from jose import JWTError,  jwt
-from passlib.context import CryptContext
+from jose import JWTError, jwt
 from app.core.config import settings
 
-pwd_context = CryptContext(schemad=["bcrypt"], deprecated="auto")
-
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
 def verify_password(plain: str, hashed: str) -> bool:
-    return pwd_context.verify(plain, hashed)
+    return bcrypt.checkpw(plain.encode("utf-8"), hashed.encode("utf-8"))
 
-def create_access_token(data: dict) ->str:
+def create_access_token(data: dict) -> str:
     payload = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
     payload.update({"exp": expire})
