2674ef83f6216222282db1f734410b78ae099dcf
Ilman Fazny <83239298+Ilman-Fazny@users.noreply.github.com>
2026-06-12 02:12:35 +0530

    feat: resend config added

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     secret_key: str
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
+    resend_api_key: str
+    from_email: str
 
     class Config:
         env_file = ".env"
